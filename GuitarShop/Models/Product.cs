538e1b8ab81439cda20f329d38e49e607c284ae6
Debo <unconfigured@null.spigotmc.org>
2021-08-30 23:30:55 -0400

    Add project files.

---
+++ GuitarShop/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace GuitarShop.Models
+{
+
+    //Model class for the product
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Slug => Name.Replace(' ', '-');
+    }
+}
